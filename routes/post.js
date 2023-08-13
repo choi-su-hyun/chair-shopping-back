639bec0f287cdff12ceb156aa7a23e874a53a243
최수현 <choi45769@gmail.com>
2023-08-13 23:56:11 +0900

    상세페이지 내용 전송 api 제작

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var db = require("../lib/db");
 
-//상품 list 전달하기 서버 api
+//상품 list 전송
 router.get("/product-list", function (req, res) {
   db.query(
     `SELECT * FROM product INNER JOIN product_category ON product.category_idx = product_category.idx INNER JOIN image ON product.idx = image.product_idx`,
@@ -21,6 +21,7 @@ router.get("/product-list", function (req, res) {
   );
 });
 
+//상품 카테고리 탭 데이터 전송
 router.get("/product-category-tab", function (req, res) {
   db.query(`SELECT * FROM product_category`, function (err, result) {
     console.log(result);
@@ -36,6 +37,7 @@ router.get("/product-category-tab", function (req, res) {
   });
 });
 
+//특정 카테고리를 조건으로 상품 리스트 전송
 router.post("/product-category-data", function (req, res) {
   const post = req.body;
   console.log(post);
@@ -55,6 +57,29 @@ router.post("/product-category-data", function (req, res) {
         });
       }
       console.log(result[0]);
+      res.status(200).json({
+        message: "SUCCESS",
+        contents: result,
+      });
+    }
+  );
+});
+
+//상세페이지 내용 전송
+router.post("/product-detail-data-process", function (req, res) {
+  const post = req.body;
+  db.query(
+    `SELECT * FROM product AS a INNER JOIN product_category AS b ON a.category_idx = b.idx INNER JOIN image AS c ON a.idx = c.product_idx WHERE a.idx = ?`,
+    [post.idx],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.status(404).json({
+          message: "DB_ERROR",
+        });
+      }
+      console.log(result[0]);
+      console.log(post);
       res.status(200).json({
         message: "SUCCESS",
         contents: result,
