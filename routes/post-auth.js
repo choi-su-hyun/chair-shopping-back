f7d9863740c743ef18cbc11b9f3f8ef498f0b508
최수현 <choi45769@gmail.com>
2023-10-11 12:59:41 +0900

    장바구니 api 구현

--- routes/post-auth.js
+++ routes/post-auth.js
@@ -2,15 +2,142 @@ var express = require("express");
 var router = express.Router();
 var db = require("../lib/db");
 
+//카트 DB에 상품 추가
 router.post("/insert-to-cart-process", function (req, res) {
   const post = req.body;
-  // const userData = req.user;
-  // console.log("post 내용", post, "user 내용", userData);
-  // db.query(`INSERT INTO cart(product_idx, user_idx, cart_qty, option_idx)`, [
-  //   post.productIdx,
-  // ]);
+  const userData = req.user;
+  console.log("post 내용", post, "user 내용", userData);
+  db.query(
+    `INSERT INTO cart(product_idx, user_idx, cart_qty, option_idx) VALUES(?,?,?,?)`,
+    [post.productIdx, userData.idx, post.quantity, post.productOption],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        res.status(500).json({
+          message: "DB_ERROR",
+          err,
+        });
+      }
+      return res.status(200).json({
+        message: "SUCCESS_ADD_TO_CART",
+        content: result,
+      });
+    }
+  );
+});
+
+//카트 리스트 정리 후 전달
+router.get("/get-cart-list-process", function (req, res) {
+  const userData = req.user;
+  console.log("user 내용", userData);
+  db.query(
+    `SELECT * FROM cart where user_idx=?`,
+    [userData.idx],
+    function (err, rows) {
+      if (err) {
+        console.log(err);
+        return res.status(404).json({
+          message: "DB_ERROR",
+          err,
+        });
+      }
+      // console.log("rows 값", rows);
+
+      db.query(
+        `SELECT cart.idx, product.idx as productId, product.product_name, product.product_discount, product.product_price, product_category.category_name, image.image_thumnail_path, product_option.idx as optionId, product_option.option_name, cart.cart_qty FROM cart 
+        JOIN product ON cart.product_idx=product.idx 
+        JOIN image ON image.product_idx=product.idx 
+        JOIN product_option ON cart.option_idx=product_option.idx
+        JOIN product_category ON product.category_idx=product_category.idx WHERE cart.user_idx=?`,
+        [userData.idx],
+        function (err, rows2) {
+          if (err) {
+            console.log(err);
+            return res.status(404).json({
+              message: "DB_ERROR",
+              err,
+            });
+          }
+          console.log("rows2 값", rows2[0]);
+
+          res.status(200).json({
+            message: "연결 완료",
+            content: rows2,
+          });
+        }
+      );
+    }
+  );
+});
+
+//카트의 상품 수량 1 추가
+router.post("/increase-cart-inventory", function (req, res) {
+  const post = req.body;
+  const userData = req.user;
+  console.log("post 값", post);
+  db.query(
+    `UPDATE cart SET cart_qty=cart_qty + 1 WHERE user_idx=? AND product_idx=?`,
+    [userData.idx, post.idx],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.status(404).json({
+          message: "DB_ERROR",
+          err,
+        });
+      }
+      res.status(200).json({
+        message: "연결 완료",
+        content: result,
+      });
+    }
+  );
+});
+
+//개인용 카트 DB 삭제
+router.delete("/delete-cart", function (req, res) {
+  const userData = req.user;
+  db.query(
+    `DELETE FROM cart WHERE user_idx=?`,
+    [userData.idx],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.status(404).json({
+          message: "DB_ERROR",
+          err,
+        });
+      }
+      res.status(200).json({
+        message: "DELETE_SUCCESS",
+        content: result,
+      });
+    }
+  );
+});
+
+//개인용 카트 DB 내용중 선택한 컬럼만 삭제
+router.delete("/delete-selected-cart", function (req, res) {
+  const userData = req.user;
+  const post = req.body;
+  console.log("userData, post", userData, post);
+  for (let i of post.selectedData) {
+    db.query(
+      `DELETE FROM cart WHERE user_idx=? AND idx=?`,
+      [userData.idx, i],
+      function (err, result) {
+        if (err) {
+          console.log(err);
+          return res.status(404).json({
+            message: "DB_ERROR",
+            err,
+          });
+        }
+      }
+    );
+  }
   res.status(200).json({
-    message: "연결 완료",
+    message: "DELETE_SUCCESS",
   });
 });
 
--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,7 @@ router.get("/product-list", function (req, res) {
   db.query(
     `SELECT * FROM product INNER JOIN product_category ON product.category_idx = product_category.idx INNER JOIN image ON product.idx = image.product_idx`,
     function (err, result) {
-      console.log(result);
+      // console.log(result);
       if (err) {
         return res.status(404).json({
           message: "DB_ERROR",
@@ -24,7 +24,7 @@ router.get("/product-list", function (req, res) {
 //상품 카테고리 탭 데이터 전송
 router.get("/product-category-tab", function (req, res) {
   db.query(`SELECT * FROM product_category`, function (err, result) {
-    console.log(result);
+    // console.log(result);
     if (err) {
       return res.status(404).json({
         message: "DB_ERROR",
@@ -40,7 +40,7 @@ router.get("/product-category-tab", function (req, res) {
 //특정 카테고리를 조건으로 상품 리스트 전송
 router.post("/product-category-data", function (req, res) {
   const post = req.body;
-  console.log(post);
+  // console.log(post);
   db.query(
     `SELECT * FROM product INNER JOIN product_category ON product.category_idx = product_category.idx INNER JOIN image ON product.idx = image.product_idx WHERE category_idx=?`,
     [post.category_idx],
@@ -56,7 +56,7 @@ router.post("/product-category-data", function (req, res) {
           contents: result,
         });
       }
-      console.log(result[0]);
+      // console.log(result[0]);
       res.status(200).json({
         message: "SUCCESS",
         contents: result,
@@ -69,7 +69,9 @@ router.post("/product-category-data", function (req, res) {
 router.post("/product-detail-data-process", function (req, res) {
   const post = req.body;
   db.query(
-    `SELECT * FROM product AS a INNER JOIN product_category AS b ON a.category_idx = b.idx INNER JOIN image AS c ON a.idx = c.product_idx WHERE a.idx = ?`,
+    `SELECT a.idx, a.category_idx, b.category_name, a.product_name, a.product_description, a.product_discount, a.product_price, c.image_thumnail_path, c.image_detail_path  FROM product AS a 
+    INNER JOIN product_category AS b ON a.category_idx = b.idx 
+    INNER JOIN image AS c ON a.idx = c.product_idx WHERE a.idx = ?`,
     [post.idx],
     function (err, result) {
       if (err) {
@@ -78,8 +80,8 @@ router.post("/product-detail-data-process", function (req, res) {
           message: "DB_ERROR",
         });
       }
-      console.log(result[0]);
-      console.log(post);
+      // console.log(result[0]);
+      // console.log(post);
       res.status(200).json({
         message: "SUCCESS",
         contents: result,
@@ -88,6 +90,7 @@ router.post("/product-detail-data-process", function (req, res) {
   );
 });
 
+//해당 상품의 옵션 리스트 전달 api
 router.post("/product-option-list", function (req, res) {
   const post = req.body;
   db.query(
@@ -107,7 +110,20 @@ router.post("/product-option-list", function (req, res) {
       });
     }
   );
-  console.log(post);
+  // console.log(post);
+});
+
+//카트DB에 저장 api
+router.post("/insert-to-cart-process", function (req, res) {
+  const post = req.body;
+  const userData = req.user;
+  console.log("post 내용", post, "user 내용", userData);
+  // db.query(`INSERT INTO cart(product_idx, user_idx, cart_qty, option_idx)`, [
+  //   post.productIdx,
+  // ]);
+  res.status(200).json({
+    message: "연결 완료",
+  });
 });
 
 module.exports = router;
